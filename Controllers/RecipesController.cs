da5d1740b1d4292c4e3cfd92186977f9ef89746f
Laura Mint <laura@mintflavour.com>
2023-10-11 16:55:30 +0200

    HttpGet GetDishes()

---
+++ Controllers/RecipesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RecipesWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        [HttpGet]
+        public string[] GetDishes()
+        {
+            string[] dishes = { "Oxtail", "Curry Chicken", "Dumplings" };
+            return dishes;
+        }
+    }
+}
