894fbe8bf266b999b50ec2dbaa0e97871fe0128a
Laura Mint <laura@mintflavour.com>
2023-10-12 18:50:35 +0200

    DbSettings added

---
+++ Config/DbSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace RecipesWebApi.Config
+{
+    public class DbSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string CollectionName { get; set; }
+    }
+}
