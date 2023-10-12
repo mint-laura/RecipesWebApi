7575ce3643bff83d822ae1ab3ec6c065c7743fd7
Laura Mint <laura@mintflavour.com>
2023-10-12 19:00:15 +0200

    Link to db

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using RecipesWebApi.Config;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<DbSettings>(builder.Configuration.GetSection("MongoDatabase"));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
