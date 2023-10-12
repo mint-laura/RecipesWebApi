c795478de4af6788bf899c5158703b5d6595b445
Laura Mint <laura@mintflavour.com>
2023-10-12 09:18:14 +0200

    DbContext added

---
+++ Data/RecipesContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RecipesWebApi.Data
+{
+    public class RecipesContext : DbContext
+    {
+        public RecipesContext(DbContextOptions<RecipesContext> options) : base(options)
+        {
+        
+        }
+        
+      
+    }
+}
